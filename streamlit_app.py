7124f3f3402b0c2a91e837ca01f540a87c981b1d
Pesterx <106877723+Pesterx@users.noreply.github.com>
2026-04-26 12:13:40 +0200

    fix2

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ if prompt := st.chat_input():
     client = OpenAI(api_key=api_key, base_url=base_url)
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
-    response = client.chat.completion.create(
+    response = client.chat.completions.create(
         model= selected_model,
         messages=st.session_state.messages 
         )
