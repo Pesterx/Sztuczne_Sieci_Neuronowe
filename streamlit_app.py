42612ef9808a3b344497c9c666890020595aa01f
Pesterx <106877723+Pesterx@users.noreply.github.com>
2026-04-26 12:02:01 +0200

    added model

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ st.set_page_config(layout="wide", page_title="Gemini chatbot app")
 st.title("Gemini chatbot app")
 
 api_key, base_url = st.secrets["API_KEY"], st.secrets["BASE_URL"]
-selected_model = 
+selected_model = "gemini-2.5-flash"
 
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?."}]
